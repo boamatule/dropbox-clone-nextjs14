abc36c0f896b816f215b8325ccb661c7670ebf0e
boa <boa.matule@gmail.com>
2023-11-30 20:38:33 +0100

    Implements skeleton loader; implements table view for all the files; builds columns for the table view; Builds Table wrapper; Setups Type definitions

--- components/DropZone.tsx
+++ components/DropZone.tsx
@@ -1,71 +1,72 @@
 "use client";
-import { useState } from "react";
-import { cn } from "@/lib/utils";
-import DropZoneComponent from "react-dropzone";
-import { useUser } from "@clerk/nextjs";
-import { addDoc, collection, doc, serverTimestamp, updateDoc } from "firebase/firestore";
-import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { db, storage } from "@/firebase";
+import { cn } from "@/lib/utils";
+import { useUser } from "@clerk/nextjs";
+import {
+	addDoc,
+	collection,
+	doc,
+	serverTimestamp,
+	updateDoc,
+} from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { useState } from "react";
+import DropZoneComponent from "react-dropzone";
 
 const DropZone = () => {
-  const [loading, setLoading] = useState(false);
-  const { isLoaded, isSignedIn, user }= useUser();
+	const [loading, setLoading] = useState(false);
+	const { user } = useUser();
 
-  const onDrop = (acceptedFiles: File[]) => {
-    acceptedFiles.forEach((file) => {
-      const reader = new FileReader();
+	const onDrop = (acceptedFiles: File[]) => {
+		acceptedFiles.forEach((file) => {
+			const reader = new FileReader();
 
-      reader.onabort = () => console.log("file reading was aborted");
-      reader.onerror = () => console.log("file reading has failed");
-      reader.onload = async () => {
-        await uploadPost(file);
-      };
-      reader.readAsArrayBuffer(file);
-    }
-  )}
+			reader.onabort = () => console.log("file reading was aborted");
+			reader.onerror = () => console.log("file reading has failed");
+			reader.onload = async () => {
+				await uploadPost(file);
+			};
+			reader.readAsArrayBuffer(file);
+		});
+	};
 
-  const uploadPost = async (selectedFile: File) => {
-    if (loading) return;
-    if (!user) return;
+	const uploadPost = async (selectedFile: File) => {
+		if (loading) return;
+		if (!user) return;
 
-    setLoading(true);
-    // addDoc => users/userId/posts
-    const docRef = await addDoc(collection(db, "users", user.id, "files"), {
-      userId: user.id,
-      fileName: selectedFile.name,
-      fullName: user.fullName,
-      profileImage: user.imageUrl,
-      fileSize: selectedFile.size,
-      fileType: selectedFile.type,
-      createdAt: serverTimestamp(),
-    });
+		setLoading(true);
+		// addDoc => users/userId/posts
+		const docRef = await addDoc(collection(db, "users", user.id, "files"), {
+			userId: user.id,
+			filename: selectedFile.name,
+			fullName: user.fullName,
+			profileImage: user.imageUrl,
+			size: selectedFile.size,
+			file: selectedFile.type,
+			timestamp: serverTimestamp(),
+		});
 
-    const imageRef = ref(storage, `users/${user.id}/files/${docRef.id}`);
-    
-    uploadBytes(imageRef, selectedFile).then(async (snapshot) => {
-      const downloadUrl = await getDownloadURL(imageRef);
+		const imageRef = ref(storage, `users/${user.id}/files/${docRef.id}`);
 
-      await updateDoc(doc(db, "users", user.id, "files", docRef.id), {
-        downloadUrl: downloadUrl,
-      });
-    })
-    setLoading(false);
-  }
+		uploadBytes(imageRef, selectedFile).then(async (snapshot) => {
+			const downloadUrl = await getDownloadURL(imageRef);
 
-  // maz file size 20MB
-  const maxSize = 20971520;
+			await updateDoc(doc(db, "users", user.id, "files", docRef.id), {
+				downloadUrl: downloadUrl,
+			});
+		});
+		setLoading(false);
+	};
+
+	// maz file size 20MB
+	const maxSize = 20971520;
 
 	return (
-		<DropZoneComponent
-			minSize={0}
-			maxSize={maxSize}
-			onDrop={onDrop}
-		>
+		<DropZoneComponent minSize={0} maxSize={maxSize} onDrop={onDrop}>
 			{({
 				getRootProps,
 				getInputProps,
 				isDragActive,
-				isDragAccept,
 				isDragReject,
 				fileRejections,
 			}) => {
@@ -74,13 +75,15 @@ const DropZone = () => {
 
 				return (
 					<section className="m-4">
-						<div 
-              {...getRootProps()}
-              className={cn(
-                "w-full h-52 flex justify-center items-center border-dashed rounded-lg text-center p-5",
-                isDragActive ? "bg-[#035FFE] text-white animate-pulse" : "bg-slate-100/50 dark:bg-slate-800/80 text-slate-400"
-              )}
-            >
+						<div
+							{...getRootProps()}
+							className={cn(
+								"w-full h-52 flex justify-center items-center border-dashed rounded-lg text-center p-5",
+								isDragActive
+									? "bg-[#035FFE] text-white animate-pulse"
+									: "bg-slate-100/50 dark:bg-slate-800/80 text-slate-400",
+							)}
+						>
 							<input {...getInputProps()} />
 							{!isDragActive && "Click here or drop a file to upload!"}
 							{isDragActive && !isDragReject && "Drop it like it's hot!"}
---
+++ components/table/columns.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { FileType } from "@/typings";
+import { ColumnDef } from "@tanstack/react-table";
+import { FileIcon, defaultStyles } from 'react-file-icon';
+import prettyBytes from "pretty-bytes";
+import { COLOR_EXTENSION_MAP } from "@/constants";
+
+export const columns: ColumnDef<FileType>[] = [
+  {
+    accessorKey: "type",
+    header: "Type",
+    cell: ({ renderValue, ...props }) => {
+      const type = renderValue() as string;
+
+      // Check if type is not null and has the expected format
+      if (type && type.includes('/')) {
+        const extension: string = type.split('/')[1];
+        return (
+          <div className="w-10">
+            <FileIcon 
+              extension={extension}
+              labelColor={COLOR_EXTENSION_MAP[extension]}
+              //@ts-ignore
+              {...defaultStyles[extension]}
+            />
+          </div>
+        );
+      } else {
+        // Handle the case when type is null or doesn't have the expected format
+        return <div className="w-10">Invalid Type</div>;
+      }
+    }
+  },
+	{
+		accessorKey: "filename",
+		header: "Filename",
+	},
+	{
+		accessorKey: "timestamp",
+		header: "Date Added",
+	},
+	{
+		accessorKey: "size",
+		header: "Size",
+		cell: ({ renderValue, ...props }) => {
+      // Check if renderValue() is not null before passing it to prettyBytes
+      const size = renderValue() as number;
+      return <span>{size !== null ? prettyBytes(size) : 'Invalid Size'}</span>;
+    },
+	},
+	{
+		accessorKey: "downloadURL",
+		header: "Link",
+		cell: ({ renderValue, ...props }) => (
+			<a
+				href={renderValue() as string}
+				target="_blank"
+				rel="noreferrer"
+				className="text-blue-500 underline hover:text-blue-600"
+			>
+				Donwload
+			</a>
+		),
+	},
+];
